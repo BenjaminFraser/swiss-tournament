d61bafa5bb235a11598efe604228023086a1db1d
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2015-10-11 11:35:32 +0100

    Initial push of fullstack repo.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,11 @@
 # 
 
 import time
+import psycopg2
+import bleach
+
 
 ## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +18,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    select_all_posts = "SELECT content, time FROM posts ORDER BY time desc"
+    c.execute(select_all_posts)
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall()]
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +33,11 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    cleaned_content = bleach.clean(content, strip=True)
+    insert_into = "INSERT INTO posts (content) VALUES (%s)"
+    c.execute(insert_into, (cleaned_content,))
+    DB.commit()
+    DB.close()
+
